871c210834a7ddf0132648255d112dd8a4e3f61f
ktoqaloglu <sector3hd@gmail.com>
2024-03-20 13:38:20 -0400

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import { ServerApiVersion } from 'mongodb';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 
 dotenv.config();
 
@@ -15,10 +18,11 @@ mongoose
 });
 
 const app = express();
-
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
