e6e81488aa6c6d5c71f18add88446a18676dfb78
ktoqaloglu <sector3hd@gmail.com>
2024-03-27 15:13:22 -0400

    create post api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { application } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { ServerApiVersion } from 'mongodb';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 
 
@@ -28,6 +29,7 @@ app.listen(3001, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
