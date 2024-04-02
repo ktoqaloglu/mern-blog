6b6a65ddb464312946c50d4c001f0a97b3bc1500
ktoqaloglu <sector3hd@gmail.com>
2024-04-02 19:19:26 -0400

    complated the dashboard page

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from 'flowbite-react';
-import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
+import { HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
@@ -38,6 +38,13 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
             <Sidebar.ItemGroup className='flex flex-col gap-1'>
+            {currentUser.isAdmin && (
+                <Link to='/dashboard?tab=dash'>
+                <Sidebar.Item active={tab == 'dash' || !tab} icon={HiChartPie} labelColor = 'dark' as='div'>
+                    Dashboard
+                </Sidebar.Item>
+              </Link>  
+            )}
                 <Link to='/dashboard?tab=profile'>
                     <Sidebar.Item active={tab == 'profile'} icon={HiUser} label= {currentUser.isAdmin ? 'Admin' : 'User'} labelColor = 'dark' as='div'>
                         Profile
