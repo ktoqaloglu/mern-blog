7be31c6e7d31286a387d2821d2f8123bb16ee4bf
ktoqaloglu <sector3hd@gmail.com>
2024-04-03 14:00:05 -0400

    add pre installition

--- api/index.js
+++ api/index.js
@@ -1,13 +1,12 @@
-import express, { application } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { ServerApiVersion } from 'mongodb';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 import commentRoutes from './routes/comment.route.js';
-
+import path from 'path';
 
 dotenv.config();
 
@@ -28,10 +27,18 @@ app.listen(3001, () => {
     console.log('Server is running on port 3001!');
 });
 
+const __dirname = path.resolve();
+
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=> {
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
