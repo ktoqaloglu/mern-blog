ca6481adfd06449c4dd92b5eaa82b4dc48d3b73c
ktoqaloglu <sector3hd@gmail.com>
2024-04-03 13:52:28 -0400

    complated to search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,11 +5,23 @@ import {FaMoon, FaSun} from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import { signoutSuccess } from '../redux/user/userSlice';
+import { useEffect, useState } from 'react';
 export default function Header() {
     const path = useLocation().pathname;
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const { currentUser } = useSelector(state => state.user);
+    const [searchTerm, setSearchTerm] = useState('');
     const { theme } = useSelector(state => state.theme);
+    const location = useLocation();
+    
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if(searchTermFromUrl){
+            setSearchTerm(searchTermFromUrl);
+        }
+    },[location.search]);
 
     const handleSignout = async () => {
         try {
@@ -26,18 +38,28 @@ export default function Header() {
           console.log(error.message);
         }
       }
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('searchTerm',searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+      }
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Kamil's</span>
             Blog
         </Link>
-        <form>
+        <form onSubmit={handleSubmit}>
             <TextInput 
                 type='text'
                 placeholder='Search...'
                 rightIcon={AiOutlineSearch}
                 className='hidden lg:inline'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
             />
             <Button className='w-12 h-10 lg:hidden' color='gray' pill>
                 <AiOutlineSearch />
